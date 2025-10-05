6259db053a381ceef2b35ef8332bd38c70e0ef50
cauan <cauanbarros14@gmail.com>
2025-10-05 13:53:50 -0300

    function for findElement

--- wallet-dev.finance/learn-functions/script.js
+++ wallet-dev.finance/learn-functions/script.js
@@ -1,2 +1,13 @@
 const trybeBanckCustomers = ["Oliva", "Nat", "Gus"];
 const pirilampoCustomers = ["Noel", "Carol", "Angelo", "Kissila"];
+
+
+function findElement(array, element) {
+    for(let index = 0; index < array.length; index += 1){
+        if(element == array[index]) {
+            console.log("o nome", array[index])
+        } 
+    }
+}
+
+findElement(trybeBanckCustomers, "Nat")
