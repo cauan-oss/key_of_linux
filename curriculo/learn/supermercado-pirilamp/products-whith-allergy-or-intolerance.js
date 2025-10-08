bd43c30b335b024d35c43d33170c83153794cc12
cauan <cauanbarros14@gmail.com>
2025-10-08 12:14:51 -0300

    to fixed

--- curriculo/learn/supermercado-pirilamp/products-whith-allergy-or-intolerance.js
+++ curriculo/learn/supermercado-pirilamp/products-whith-allergy-or-intolerance.js
@@ -2,30 +2,18 @@ const stockProducts = require("./data.json");
 
 const getProductsWithAllergyOrIntoleranceInfo = () => {
     let productsWithIntolerance = [];
-    let compara = [];
-    let listFinish = []
-    const dataWithIntolerance = {
-        description: '',
-        formattedPrice: '',
-        allergyOrIntoleranceMessage: ''
-    }
+   
     for (let index = 0; index < stockProducts.length; index += 1) {
         if (stockProducts[index].allergyOrIntolerance !== undefined) {
-            productsWithIntolerance.push(stockProducts[index])
-            compara.push(stockProducts[index].allergyOrIntolerance);
+                productsWithIntolerance.push({
+                    description: stockProducts[index].description,
+                    formattedPrice: stockProducts[index].price,
+                    allergyOrIntoleranceMessage: `Pode conter: ${stockProducts[index].allergyOrIntolerance}`
+                })
         }
     }
-    /* Percorre o array de products e adicionar dentro de dataWith o valores e compare */
-    for (let index = 0; index < productsWithIntolerance.length; index += 1) {
-           if(productsWithIntolerance[index].allergyOrIntolerance === compara[index]) {
-             dataWithIntolerance.description = productsWithIntolerance[index].description
-             dataWithIntolerance.formattedPrice = productsWithIntolerance[index].price
-             dataWithIntolerance.allergyOrIntoleranceMessage = compara[index]
-             listFinish.push(dataWithIntolerance);
-            }
-    }
 
-    return listFinish
+    return productsWithIntolerance
 };
 
 console.log(getProductsWithAllergyOrIntoleranceInfo())
