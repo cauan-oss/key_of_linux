c672af82c8ee07f4a9aa6661ca1326530bd13c48
cauan <cauanbarros14@gmail.com>
2025-10-08 13:13:13 -0300

    almost finish

--- curriculo/learn/supermercado-pirilamp/productsRichVitamin.js
+++ curriculo/learn/supermercado-pirilamp/productsRichVitamin.js
@@ -1,5 +1,10 @@
 const stockProducts = require("./data.json");
 
 function getProductsRichVitain () {
+    let listWithVitamin = []
+     for(let index = 0; index < stockProducts.length; index += 1){
+          if(stockProducts[index].vitamins !== undefined) {
 
+          }
+     }
 }
