76af88dc73658c2bf16f8a622ca79d502e8b09af
cauan <cauanbarros14@gmail.com>
2025-10-10 12:39:55 -0300

    class wiht nexous

--- learningDom/script.js
+++ learningDom/script.js
@@ -1,5 +1,8 @@
 class Botton {
     idBotton = "";
+    constructor (idBotton) {
+           this.idBotton = idBotton
+    }
     changeText (newText) {
         const botton = document.getElementById(this.idBotton)
         botton.innerText = newText
@@ -9,12 +12,24 @@ class Botton {
          change.style.backgroundColor = color
          change.style.textAlign = alinhe
     }
+    getButton() {
+       const returnButton = document.getElementById(this.idBotton)
+       return returnButton
+    }
 }
-const botao1 = new Botton()
 
-botao1.idBotton = "title";
+
+const botao1 = new Botton('title')
+const text = new Botton('name');
+text.changeText('Cauan')
+text.changeBackground('yellow', 'center');
+
+text.getButton().addEventListener('mouseover', () => {
+   text.changeBackground('blue', 'center')       
+});
+
 botao1.changeText('Hello world');
-botao1.changeBackground('red', 'center');
+botao1.changeBackground('red', 'right');
 
 
 
