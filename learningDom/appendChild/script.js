4d161268e2bb90349f99b70154a44f2adb931b31
cauan <cauanbarros14@gmail.com>
2025-10-14 13:41:43 -0300

    learning appendChild

--- learningDom/appendChild/script.js
+++ learningDom/appendChild/script.js
@@ -1,12 +1,20 @@
 const InputOfUrl = document.getElementById("urlInput");
 const InputOLegend = document.getElementById("legendaInput");
 const button = document.getElementById("adicionarBtn");
+const galeryContainer = document.getElementById("galeriaContainer");
+
 
 button.addEventListener('click', (param) => {
     /* InputOfUrl.value
     InputOLegend.value */
-    const insertCard = document.createElement('div').classList.add('item-galeria');
+    const insertCard = document.createElement('div')
+    insertCard.classList.add('item-galeria');
     const imagem = document.createElement('img');
+    imagem.src = InputOfUrl.value
     const legend = document.createElement('p');
+    legend.textContent = InputOLegend.value
+    insertCard.appendChild(imagem)
+    insertCard.appendChild(legend)
+    galeryContainer.appendChild(insertCard)
     console.log()
 })
