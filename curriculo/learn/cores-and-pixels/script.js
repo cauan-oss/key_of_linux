76d7e26db8ef398b275f9a4965081d18275e6978
cauan <cauanbarros14@gmail.com>
2025-10-14 08:48:22 -0300

    restarting colors

--- curriculo/learn/cores-and-pixels/script.js
+++ curriculo/learn/cores-and-pixels/script.js
@@ -8,44 +8,12 @@ function createColorRandom() {
 }
 
 
-
-const listValuesColor = [];
 const squares = document.getElementsByClassName("color");
 function insertColorInTheSquares() {
-    for (let index = 1; index < squares.length; index += 1) {
-        squares[index].style.backgroundColor = createColorRandom()
-        listValuesColor.push(squares[index].style.backgroundColor)
-
+    for(let index = 0; index < squares.length; index += 1) {
+        squares[index].style.backgroundColor = createColorRandom();
     }
-    console.log(listValuesColor)
-    squares[0].style.backgroundColor = 'black'
 }
-
-
-/* salvar apenas o numeros do rbg no localStorage e depois chamar esse numeros nos quadrados */
-
-
-
-function buttonColorRandom() {
-    const button = document.getElementById("button-random-color")
-    button.addEventListener('click', () => {
-        insertColorInTheSquares()
-        localStorage.setItem('colorPalette', listValuesColor)
-        for (let index = 0; index < listValuesColor.length; index += 1) {
-            for (let indice = 0; indice < squares.length; indice += 1) {
-                if (listValuesColor[index] !== squares[index]) {
-                    listValuesColor.splice(listValuesColor[index])
-                }
-                if(listValuesColor[index] == squares[index]) {
-                    squares[index].style.backgroundColor = localStorage.getItem('colorPalette')
-                }
-            }
-        }
-    })
-
-
-}
-buttonColorRandom()
 insertColorInTheSquares()
 /* savingInTheLocal() */
 /* console.log('aqui', savingInTheLocal()) */
