d0a8c7f349aaf09a2cd2166e03549da3479afb47
cauan <cauanbarros14@gmail.com>
2025-10-14 13:25:53 -0300

    define classes for createElement

---
+++ learningDom/appendChild/script.js
@@ -0,0 +1,12 @@
+const InputOfUrl = document.getElementById("urlInput");
+const InputOLegend = document.getElementById("legendaInput");
+const button = document.getElementById("adicionarBtn");
+
+button.addEventListener('click', (param) => {
+    /* InputOfUrl.value
+    InputOLegend.value */
+    const insertCard = document.createElement('div').classList.add('item-galeria');
+    const imagem = document.createElement('img');
+    const legend = document.createElement('p');
+    console.log()
+})
