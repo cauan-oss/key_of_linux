7c3beb576a4f4fa7b5906fe8baf64d338b8d955f
cauan <cauanbarros14@gmail.com>
2025-10-14 09:49:20 -0300

    local correct

--- curriculo/learn/cores-and-pixels/script.js
+++ curriculo/learn/cores-and-pixels/script.js
@@ -1,4 +1,5 @@
 
+const squares = document.getElementsByClassName("color");
 function createColorRandom() {
     const r = Math.ceil(Math.random() * 255);
     const g = Math.ceil(Math.random() * 255);
@@ -8,16 +9,48 @@ function createColorRandom() {
 }
 
 
-const squares = document.getElementsByClassName("color");
+let listValuesForLocal = [];
 function insertColorInTheSquares() {
-    for(let index = 0; index < squares.length; index += 1) {
-        squares[index].style.backgroundColor = createColorRandom();
+    for(let index = 1; index < squares.length; index += 1) {
+        squares[index].style.backgroundColor = createColorRandom()
     }
+    squares[0].style.backgroundColor = 'black'
 }
+
+/*
+ 1  quando abrir a pagina gere cores aleatorias ok
+ 2- quando eu clicar no botao crie novas cores - ok
+ 3 quando eu clicar no botao salve no localStorage -ok 
+ 4 quando eu recarregar a pagina retorne um console com local em formato de array
+*/
+function buttonCreateNewColors() {
+    insertColorInTheSquares()
+    let saveOfLocal = [];
+    for(let index = 0; index < squares.length; index += 1) {
+         saveOfLocal.push(squares[index].style.backgroundColor)
+    }
+    let convertListInString = JSON.stringify(saveOfLocal)
+    localStorage.setItem('colorPalette', convertListInString);
+    console.log(convertListInString)
+}
+
+
+function getColorOfLocal () {
+    let getOfLocal = JSON.parse(localStorage.getItem('colorPalette'));
+    for(let i in getOfLocal) {
+        squares[i].style.backgroundColor = getOfLocal[i]
+    }
+
+}
+
+
+const button = document.getElementById("button-random-color");
+button.addEventListener('click', () => {
+   buttonCreateNewColors()
+})
 insertColorInTheSquares()
-/* savingInTheLocal() */
-/* console.log('aqui', savingInTheLocal()) */
-//console.log(savingInTheLocal())
+getColorOfLocal()
+
 
 
 
