230fb9ae8b868ece24a00bfcefd27b5077a38a35
cauan <cauanbarros14@gmail.com>
2025-10-21 10:40:12 -0300

    learning forOf

--- curriculo/learn/car-Clients/script.js
+++ curriculo/learn/car-Clients/script.js
@@ -1,62 +1,70 @@
 class Veiculo {
-    //atributos 
-    #marca 
-    #ano
-    #preco
+  //atributos 
+  #marca
+  #ano
+  #preco
 
-    constructor(marca, ano, preco) {
-        this.#marca = marca
-        this.#ano = ano
-        this.#preco = preco 
-    }
+  constructor(marca, ano, preco) {
+    this.#marca = marca
+    this.#ano = ano
+    this.#preco = preco
+  }
 
-    alterarPreco(newPrice) {
-         this.#preco = newPrice
+  alterarPreco(newPrice) {
+    this.#preco = newPrice
+  }
+  getPreco() {
+    return this.#preco
+  }
+  getDetalhes() {
+    return `${this.#marca}, ${this.#ano}, ${this.#preco}`
+  }
+  calculaIpva(tabelaIpva) {
+    let ipva = 0;
+    if (this.#ano > 2005) {
+      ipva = tabelaIpva.taxaAcima2005 * 100
+      return ipva
     }
-    getPreco() {
-       return this.#preco
-    }
-    getDetalhes () {
-        console.log('em cima ')
-    }
-    calculaIpva(tabelaIpva) {
-        let ipva = 0;
-        if(this.#ano > 2005) {
-          ipva =  tabelaIpva.taxaAcima2005 * 100
-          return ipva
-        }
-        if(this.#ano < 2005) {
-          ipva =  tabelaIpva.taxaAte2005 * 100
-          return ipva
-        }
-       
+    if (this.#ano < 2005) {
+      ipva = tabelaIpva.taxaAte2005 * 100
+      return ipva
     }
 
-    calcularValorSeguro(tabelaSeguro){
+  }
 
-    }
+  calcularValorSeguro(tabelaSeguro) {
+      this.#preco
+  }
 }
 
 
 class Carro extends Veiculo {
-    constructor(numeroDePortas) {
-      super(numeroDePortas)
-         this.numeroDePortas = numeroDePortas
-         
-    }
-   getDetalhes(){
+  constructor(numeroDePortas) {
+    super(numeroDePortas)
+    this.numeroDePortas = numeroDePortas
+
+  }
+  getDetalhes() {
     console.log(this.numeroDePortas);
-   }
+  }
+  calcularValorSeguro(tabelaSeguro) {
+    const calculeFatorCarro = this.getPreco() * tabelaSeguro.fatorCarro
+    return calculeFatorCarro;
+  }
 }
 
 class Moto extends Veiculo {
-     constructor(cilindrada){
-      super(cilindrada)
-      this.cilindrada = cilindrada
-     }
-     getDetalhes() {
-      console.log(this.getDetalhes)
-     }
+  constructor(cilindrada) {
+    super(cilindrada)
+    this.cilindrada = cilindrada
+  }
+  getDetalhes() {
+    console.log(this.getDetalhes)
+  }
+  calcularValorSeguro(tabelaSeguro) {
+     const calculeFatorMoto = this.getPreco() * tabelaSeguro.fatorMoto
+     return calculeFatorMoto
+  }
 }
 
 class Caminhao extends Veiculo {
@@ -67,32 +75,66 @@ class Caminhao extends Veiculo {
   getDetalhes() {
     console.log(this.capacidadeDeCarga);
   }
+
+  calcularValorSeguro(tabelaSeguro) {
+     const calculeFatorCaminhao = this.getPreco() * tabelaSeguro.fatorCaminhao
+     return calculeFatorCaminhao
+  }
+}
+
+class Cliente {
+  constructor(nome, cpf){
+    this.nome = nome;
+    this.cpf = cpf
+  }
+  getDados(){
+    return `${this.nome} e ${ this.cpf } do cliente`
+  }
+}
+
+class Concessionaria {
+  constructor() {
+    this.estoque = [];
+    this.historicoDeVendas = [];
+    this.clienteCadastrados = [];
+  }
+
+  cadastrarClientes(cliente) {
+    this.clienteCadastrados.push(cliente)
+  }
+  adicionarVeiculo() {
+
+  }
+  listarVeiculo() {
+
+  }
+  buscarPorMarca() {
+
+  }
+  venderVeiculo(modelo, cpfCliente){
+      for (const element of ) {
+        
+      }
+  }
 }
 
 
+const inserindo = new Veiculo('vw', '2003', 20000);
+console.log(inserindo.getDetalhes())
 
 
-
-
-const concessionaria = new Veiculo('vw', '2003', 20000);
-const meuCarro = new Carro(8);
-const minhaMoto = new Moto(1000)
-const meuCaminhao = new Caminhao(8000);
-
-const tabelaSeguro = { 
-base: 2000,           
-// Valor base da apólice 
-fatorCarro: 0.05,     // 5% sobre o valor do veículo 
-fatorMoto: 0.07,      
-// 7% sobre o valor do veículo 
-fatorCaminhao: 0.08   // 8% sobre o valor do veículo 
+const tabelaSeguro = {
+  base: 2000,
+  // Valor base da apólice 
+  fatorCarro: 0.05,     // 5% sobre o valor do veículo 
+  fatorMoto: 0.07,
+  // 7% sobre o valor do veículo 
+  fatorCaminhao: 0.08   // 8% sobre o valor do veículo 
 };
 
 
-concessionaria.calculaIpva(2006)
-const tabelaIpva = { 
-taxaAcima2005: 0.04,  // 4% 
-taxaAte2005: 0.025    // 2.5% 
+const tabelaIpva = {
+  taxaAcima2005: 0.04,  // 4% 
+  taxaAte2005: 0.025    // 2.5% 
 };
-concessionaria.getDetalhes()
-meuCarro.getDetalhes()
+
