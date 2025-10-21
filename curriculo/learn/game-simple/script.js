6c8bad1843ad0200f902026dc2d4e7e88a3bcb2d
cauan <cauanbarros14@gmail.com>
2025-10-21 09:06:18 -0300

    creating classes of wars

--- curriculo/learn/game-simple/script.js
+++ curriculo/learn/game-simple/script.js
@@ -1,5 +1,23 @@
 class personagem {
-    constructor(){
+    constructor(nome, vida, ataque, defesa ){
+       this.nome = nome
+       this.vida = vida 
+       this.ataque = ataque
+       this.defesa = defesa
+    }
+    atacar(alvo) {
+
+    }
+    receberDano(dano){
+      
+    }
+}
+
+class guerreiro extends personagem {
+    constructor(forca) {
+        this.forca = forca
+    }
+    ataque(pesado) {
         
     }
 }
