0f24b31b71b0055955016b1b2af62569729feec2
cauan <cauanbarros14@gmail.com>
2025-10-22 11:58:40 -0300

    simplificando a logica

--- curriculo/learn/trybeWarts/script.js
+++ curriculo/learn/trybeWarts/script.js
@@ -1,22 +1,12 @@
-const email = document.getElementsByName("email");
-const senha = document.getElementsByName("senha");
+const email = document.getElementById("email");
+const senha = document.getElementById("senha");
 const button = document.getElementById("botao");
 const userEmail = "tryber@teste.com";
 const userSenha = "12345";
 
 button.addEventListener('click', (e) => {
     e.preventDefault()
-    const user = {
-        email: '', 
-        senha: ''
-    }
-    for(let index = 0; index < email.length; index += 1) {
-       user.email = email[index].value
-    }
-     for(let index = 0; index < senha.length; index += 1) {
-       user.senha = senha[index].value
-    }
-    if(userEmail === user.email && userSenha === user.senha){
-        alert("Ola tryber")
-    }
+   if(email.value === userEmail && senha.value === userSenha){
+      alert('Ola, tryber')
+   } 
 })
