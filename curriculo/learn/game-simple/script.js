57afb19485ae8882087e9538784cc39f1dd2d5e3
cauan <cauanbarros14@gmail.com>
2025-10-24 11:40:54 -0300

    quebra cabeca

--- curriculo/learn/game-simple/script.js
+++ curriculo/learn/game-simple/script.js
@@ -1,36 +1,44 @@
 class Personagem {
-    constructor(nome, vida, ataque, defesa ){
-       this.nome = nome
-       this.vida = vida 
-       this.ataque = ataque
-       this.defesa = defesa
+    constructor(nome, vida, ataque, defesa) {
+        this.nome = nome
+        this.vida = vida
+        this.ataque = ataque
+        this.defesa = defesa
     }
-    atacar(alvo) {
 
+    /* o parametro de atacar recebe personagem */
+    atacar(alvo) {
+       alvo.receberDano(this.ataque)
     }
-    receberDano(dano){
-      
+    receberDano(dano) {
+       // console.log(` Quantidade defesa:  ${this.defesa - dano}`)
+        const calculaDefesa = this.defesa - dano
+        let calculaVida ;
+        if(calculaDefesa < 0) {
+          calculaVida = this.vida + calculaDefesa
+          console.log('vida', calculaVida);
+        }
+        if(calculaVida <= 0) {
+            console.log('Personagem morreu')
+        }
+       
     }
 }
 
 class Guerreiro extends Personagem {
     constructor(forca, nome, vida, ataque, defesa) {
         super(nome, vida, ataque, defesa)
-        this.nome = nome
         this.forca = forca
-        this.ataque = ataque
-        this.vida = vida
-        this.defesa = defesa
     }
-      atacar(alvo){
+    atacar(alvo) {
 
-      }
-      receberDano(dano){
+    }
+    receberDano(dano) {
 
-      }
+    }
 
     ataquePesado(pesado) {
-        
+
     }
 }
 
@@ -41,20 +49,27 @@ class Mago extends Personagem {
         this.magia = magia
     }
 
-    lancarFeitico(alvo){
-         
+    lancarFeitico(alvo) {
+
     }
 }
 
 class Inventario {
-     constructor() {
+    constructor() {
         this.itens = []
-     }
+    }
 
-     adicionarItem(item){
+    adicionarItem(item) {
 
-     }
-     usarItem(item, alvo) {
+    }
+    usarItem(item, alvo) {
 
-     }
+    }
 }
+
+const robervaldo = new Personagem('robervaldo', 10, 10, 50)
+const chefao = new Personagem('chefao', 20, 15, 80)
+//const mostraDano = robervaldo.receberDano(510)
+const mostraAtaque = robervaldo.atacar(chefao)
+
+console.log(mostraAtaque)
