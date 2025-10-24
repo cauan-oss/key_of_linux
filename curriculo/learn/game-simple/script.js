f0db670263cc055f23f9f2a31d397f9c0bc3d031
cauan <cauanbarros14@gmail.com>
2025-10-24 09:55:20 -0300

    super

--- curriculo/learn/game-simple/script.js
+++ curriculo/learn/game-simple/script.js
@@ -1,4 +1,4 @@
-class personagem {
+class Personagem {
     constructor(nome, vida, ataque, defesa ){
        this.nome = nome
        this.vida = vida 
@@ -13,11 +13,48 @@ class personagem {
     }
 }
 
-class guerreiro extends personagem {
-    constructor(forca) {
+class Guerreiro extends Personagem {
+    constructor(forca, nome, vida, ataque, defesa) {
+        super(nome, vida, ataque, defesa)
+        this.nome = nome
         this.forca = forca
+        this.ataque = ataque
+        this.vida = vida
+        this.defesa = defesa
     }
-    ataque(pesado) {
+      atacar(alvo){
+
+      }
+      receberDano(dano){
+
+      }
+
+    ataquePesado(pesado) {
         
     }
+}
+
+class Mago extends Personagem {
+    constructor(mana, magia) {
+        super()
+        this.mana = mana
+        this.magia = magia
+    }
+
+    lancarFeitico(alvo){
+         
+    }
+}
+
+class Inventario {
+     constructor() {
+        this.itens = []
+     }
+
+     adicionarItem(item){
+
+     }
+     usarItem(item, alvo) {
+
+     }
 }
